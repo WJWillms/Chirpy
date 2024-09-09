d4ed6ac292f2c24279ce7b513df35231c566fa85
Wade Willms <wade@otisinstruments.com>
2024-09-09 16:42:44 -0500

    Feat: Chapter 1, Part 8

--- main.go
+++ main.go
@@ -5,23 +5,22 @@ import (
 	"net/http"
 )
 
-type server struct {
-	Addr    string
-	Handler http.Handler
-}
-
 func main() {
 	// Create a new ServeMux
 	mux := http.NewServeMux()
 
-	srv := &server{
-		Addr:    ":8080",
-		Handler: mux,
-	}
+	// Define the directory to serve files from
+	staticDir := "." // Adjust this path to the directory containing your static files
+
+	// Create a FileServer handler for serving static files
+	fileServer := http.FileServer(http.Dir(staticDir))
+
+	// Use the .Handle() method to register the FileServer for the root path
+	mux.Handle("/", http.StripPrefix("/", fileServer))
 
 	httpServer := &http.Server{
-		Addr:    srv.Addr,
-		Handler: srv.Handler,
+		Addr:    ":8080",
+		Handler: mux,
 	}
 
 	// Start the server
